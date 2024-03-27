3d5f0075172df98b6fb2feb494a2484f19a39784
Fu1Yang <147630039+Fu1Yang@users.noreply.github.com>
2024-03-27 17:27:18 +0100

    **mis a jour du code**

--- src/model/Question.php
+++ src/model/Question.php
@@ -1,18 +1,1 @@
-<?php
 
-namespace App\MyQuizz;
-
-class Question
-{
-    private string $text;
-
-    public function __construct(string $text)
-    {
-        $this->text = $text;
-    }
-
-    public function getText(): string
-    {
-        return $this->text;
-    }
-}
